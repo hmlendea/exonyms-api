0e8473d5cdf720c5325f272f758468511ffdf2cf
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-05-24 16:41:43 +0300

    Name normalisation UTs

---
+++ ExonymsAPI.UnitTests/Service/NameNormaliserTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using ExonymsAPI.Service;
+
+namespace ExonymsAPI.UnitTests.Service
+{
+    public class NameNormaliserTests
+    {
+        private INameNormaliser nameNormaliser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.nameNormaliser = new NameNormaliser();
+        }
+
+        [Test]
+        [TestCase("Abati Tyndyrn", "Tyndyrn")]
+        [TestCase("Abaty Tyndyrn", "Tyndyrn")]
+        [TestCase("abbaye de Tintern", "Tintern")]
+        [TestCase("abbazia di Tintern", "Tintern")]
+        [TestCase("Klášter Tintern", "Tintern")]
+        [TestCase("Opactwo Tintern", "Tintern")]
+        [TestCase("Opatija Tintern", "Tintern")]
+        [TestCase("Tintern Abbey", "Tintern")]
+        public void GivenAName_WhenNormalisingIt_ThenAllUnwantedWordsAreRemoved(
+            string name,
+            string expectedNormalisedName)
+            => Assert.That(nameNormaliser.Normalise(string.Empty, name), Is.EqualTo(expectedNormalisedName));
+
+        [Test]
+        [TestCase("Tinternin luostari", "Tintern")]
+        public void GivenAFinnishName_WhenNormalisingIt_ThenAllUnwantedWordsAreRemoved(
+            string name,
+            string expectedNormalisedName)
+            => Assert.That(nameNormaliser.Normalise("fi", name), Is.EqualTo(expectedNormalisedName));
+    }
+}
