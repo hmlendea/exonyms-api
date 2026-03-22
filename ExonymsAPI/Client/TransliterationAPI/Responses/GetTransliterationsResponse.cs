0e2d04bafdf85bedc35cc357ac3bd82de327c614
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-22 13:55:31 +0200

    transliteration: Fix response parsing

--- ExonymsAPI/Client/TransliterationAPI/Responses/GetTransliterationsResponse.cs
+++ ExonymsAPI/Client/TransliterationAPI/Responses/GetTransliterationsResponse.cs
@@ -1,9 +1,11 @@
+using System.Text.Json.Serialization;
 using NuciAPI.Responses;
 
 namespace ExonymsAPI.Client.TransliterationAPI.Responses
 {
     public class GetTransliterationsResponse : NuciApiSuccessResponse
     {
+        [JsonPropertyName("text")]
         public string Text { get; set; }
     }
 }
