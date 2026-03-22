291f98c46111eeb489fa884535f18b4337d97047
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-22 10:21:32 +0200

    swagger: Removed Swagger

--- ExonymsAPI/Startup.cs
+++ ExonymsAPI/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace ExonymsAPI
 {
@@ -11,7 +10,6 @@ namespace ExonymsAPI
     {
         public IConfiguration Configuration { get; } = configuration;
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -27,21 +25,13 @@ namespace ExonymsAPI
                         .AllowAnyMethod();
                 });
             });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExonymsAPI", Version = "v1" });
-            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExonymsAPI v1"));
             }
 
             app.UseCors();
