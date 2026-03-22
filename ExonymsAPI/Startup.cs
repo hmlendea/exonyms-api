6c044d004533e760063d10472a1ab6cc7646d4d7
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-22 10:38:50 +0200

    nuciapi: Integrated with NuciAPI.Middleware

--- ExonymsAPI/Startup.cs
+++ ExonymsAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NuciAPI.Middleware;
 
 namespace ExonymsAPI
 {
@@ -29,6 +30,8 @@ namespace ExonymsAPI
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseNuciApiExceptionHandling();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,6 +40,10 @@ namespace ExonymsAPI
             app.UseCors();
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseNuciApiHeaderValidation();
+            app.UseNuciApiReplayProtection();
+
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
