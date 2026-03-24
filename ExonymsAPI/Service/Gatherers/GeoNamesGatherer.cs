bac37641c84ab37d245c7fdd80c149c34194544b
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-24 09:28:06 +0200

    geonames: Balance between multiple usernames

--- ExonymsAPI/Service/Gatherers/GeoNamesGatherer.cs
+++ ExonymsAPI/Service/Gatherers/GeoNamesGatherer.cs
@@ -8,6 +8,7 @@ using ExonymsAPI.Client.TransliterationAPI;
 using ExonymsAPI.Logging;
 using ExonymsAPI.Service.Models;
 using ExonymsAPI.Service.Processors;
+using NuciExtensions;
 using NuciLog.Core;
 
 namespace ExonymsAPI.Service.Gatherers
@@ -17,9 +18,19 @@ namespace ExonymsAPI.Service.Gatherers
         ITransliterationApiClient transliterationApiClient,
         ILogger logger) : IGeoNamesGatherer
     {
-        private static string GeoNamesRequestUrlFormat => "http://api.geonames.org/get?geonameId={0}&username=geonamesfreeaccountt";
+        private static string GeoNamesRequestEndpointFormat => "https://api.geonames.org/get?geonameId={0}&username={1}";
         private static string DefaultNameLanguageCode => "en";
         private static string[] IgnoredLanguageCodes => ["link", "unlc", "wkdt"];
+
+        readonly IEnumerable<string> usernames =
+        [
+            "geonamesfreeaccountt", "freacctest1", "freacctest2", "commando.gaztons", "berestesievici", "izvoli.prostagma",
+            "gesturioso", "random.name.ftw", "b75268", "b75375", "b75445", "b75445",
+            "botu0", "botu1", "botu2", "botu3", "botu4", "botu5", "botu6", "botu7", "botu8", "botu9",
+            "elBot0", "elBot1", "elBot2", "elBot3", "elBot4", "elBot5", "elBot6", "elBot7", "elBot8", "elBot9",
+            "botman0", "botman1", "botman2", "botman3", "botman4", "botman5", "botman6", "botman7", "botman8", "botman9",
+            "botean0", "botean1", "botean2", "botean3", "botean4", "botean5"
+        ];
 
         public async Task<Location> Gather(string geoNamesId)
         {
@@ -59,11 +70,17 @@ namespace ExonymsAPI.Service.Gatherers
         }
 
         async Task<Location> FetchLocation(string geoNamesId)
-        {Location location = new();
+        {
+            Location location = new();
 
-            using (HttpClient client = new())
+            using HttpClient client = new();
             {
-                HttpResponseMessage response = await client.GetAsync(string.Format(GeoNamesRequestUrlFormat, geoNamesId));
+                string geoNamesEndpoint = string.Format(
+                    GeoNamesRequestEndpointFormat,
+                    geoNamesId,
+                    usernames.GetRandomElement());
+
+                HttpResponseMessage response = await client.GetAsync(geoNamesEndpoint);
 
                 if (!response.IsSuccessStatusCode)
                 {
